8cf2d969813ff927eb29b08887e26b58305266bc
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-13 22:10:20 -0800

    retries character stat approach. moves towards refs over resources

---
+++ character/character_stats/character_stat_definition/character_stat_definition.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name CharacterStatDefinition
+
+@export var name: String = "default_stat_name"
+@export var min_value: int = 0
+@export var max_value: int = 999
+# Write more SHARED data template stuff here
+# Don't use for immediate character state
--- character/health/health.gd
+++ character/health/health.gd
@@ -7,23 +7,23 @@ signal health_depleted
 var max_health: int
 var min_health: int
 
-var _health: int
+var _current_health: int
 var current_health: int:
 	get:
-		return _health
+		return _current_health
 	set(value):
-		if value != _health:
+		if value != _current_health:
 			health_changed.emit(value)
 
-		if value > _health:
+		if value > _current_health:
 			health_increased.emit()
-		elif value < _health:
+		elif value < _current_health:
 			health_decreased.emit()
-		_health = clampi(value, min_health, max_health)
-		if _health <= min_health:
+		_current_health = clampi(value, min_health, max_health)
+		if _current_health <= min_health:
 			health_depleted.emit()
 
 func _init(p_max_health: int = 100, p_min_health: int = 0) -> void:
 	max_health = p_max_health
 	min_health = p_min_health
-	_health = max_health
+	_current_health = max_health
