26f766ff79764d9e121e958b4ca04a7f5e037f64
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2025-12-14 15:50:59 -0800

    moves back to a resource approach since i'll want these to serialize

--- character/character_stats/character_stat.gd
+++ character/character_stats/character_stat.gd
@@ -1,9 +1,9 @@
-extends RefCounted
+extends Resource
 class_name CharacterStat
 
 var stat_definition: CharacterStatDefinition
 var value: int
 
-func _init(p_stat_definition: CharacterStatDefinition, p_initial_value := 1) -> void:
+func _init(p_stat_definition := CharacterStatDefinition.new(), p_value := 1) -> void:
     stat_definition = p_stat_definition
-    value = p_initial_value
+    value = p_value
--- character/character_stats/character_stat_definition/character_stat_definition.gd
+++ character/character_stats/character_stat_definition/character_stat_definition.gd
@@ -1,8 +1,20 @@
 extends Resource
 class_name CharacterStatDefinition
 
-@export var name: String = "default_stat_name"
-@export var min_value: int = 0
-@export var max_value: int = 999
 # Write more SHARED data template stuff here
 # Don't use for immediate character state
+# Intended to be read-only at runtime. 
+# Don't think i'd ever need a new definition at runtime, 
+# they can all be made ahead of time in editor
+
+@export var _stat_name: String = "default_stat_name"
+var stat_name: String:
+    get: return _stat_name
+
+@export var _min_value: int = 0
+var min_value: int:
+    get: return _min_value
+
+@export var _max_value: int = 999
+var max_value: int:
+    get: return _max_value
