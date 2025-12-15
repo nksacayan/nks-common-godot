f5e095d47f40602bf8eab60ce171a1dcaa00d778
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-14 20:14:19 -0800

    minor refactor

--- character/character_stats/character_stat.gd
+++ character/character_stats/character_stat.gd
@@ -1,9 +1,9 @@
 extends Resource
 class_name CharacterStat
 
-var stat_definition: CharacterStatDefinition
+var definition: CharacterStatDefinition
 var value: int
 
 func _init(p_stat_definition: CharacterStatDefinition = CharacterStatDefinition.new(), p_value: int = p_stat_definition.min_value) -> void:
-    stat_definition = p_stat_definition
+    definition = p_stat_definition
     value = p_value
