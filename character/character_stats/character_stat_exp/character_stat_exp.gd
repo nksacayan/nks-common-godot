22763fed579109630734ddfcee4862d198221769
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-18 13:32:58 -0800

    starts stat exp implementation

---
+++ character/character_stats/character_stat_exp/character_stat_exp.gd
@@ -0,0 +1,25 @@
+extends Resource
+class_name CharacterStatExp
+
+@export var managed_stat: CharacterStat
+
+# "Level" will be the managed stat's value
+var _max_exp: int = 100
+@export var current_exp: int = 0:
+	get:
+		return current_exp
+	set(value):
+		if value > _max_exp:
+			# TODO: Exp scaling logic so that every level doesn't cost the same exp
+			_level_stat()
+			value -= _max_exp
+			current_exp = value
+			return
+		current_exp = value
+
+
+func _level_stat() -> void:
+	managed_stat.value += 1
+
+func _init(p_managed_stat: CharacterStat = CharacterStat.new()) -> void:
+	managed_stat = p_managed_stat
