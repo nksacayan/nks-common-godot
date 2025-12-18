dd5cef14727161addd91a8100957c1df7e0c752f
Nicholas Sacayan <nksacayan@gmail.com>
2025-12-18 15:05:53 -0800

    minor changes

--- character/character_stats/character_stat.gd
+++ character/character_stats/character_stat.gd
@@ -6,7 +6,7 @@ signal value_changed(new_value: int)
 @export var definition: CharacterStatDefinition
 @export var value: int:
     set(p_value):
-        value = p_value
+        value = clamp(p_value, definition.min_value, definition.max_value)
         value_changed.emit(value)
 
 func _init(p_stat_definition: CharacterStatDefinition = CharacterStatDefinition.new(), p_value: int = p_stat_definition.min_value) -> void:
--- character/character_stats/character_stat_exp/character_stat_exp.gd
+++ character/character_stats/character_stat_exp/character_stat_exp.gd
@@ -9,10 +9,9 @@ var _max_exp: int = 100
 	set(value):
 		if value > _max_exp:
 			# TODO: Exp scaling logic so that every level doesn't cost the same exp
+			# TODO: Handle having more exp than a single level up
 			_level_stat()
 			value -= _max_exp
-			current_exp = value
-			return
 		current_exp = value
 
 
