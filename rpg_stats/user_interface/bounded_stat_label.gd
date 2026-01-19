6ae4abf5096145169003cb003565f1071774abf0
Nicholas Sacayan <nicholas.sacayan@vetsez.com>
2026-01-19 10:55:32 -0800

    bounded stat label now connects to both values

--- rpg_stats/user_interface/bounded_stat_label.gd
+++ rpg_stats/user_interface/bounded_stat_label.gd
@@ -5,14 +5,18 @@ class_name BoundedStatLabel
 var bounded_stat: BoundedStat:
 	set(p_stat):
 		# 1. Cleanup old stat if it exists
-		if bounded_stat and bounded_stat.value_changed.is_connected(_set_label_text):
-			bounded_stat.value_changed.disconnect(_set_label_text)
+		if bounded_stat:
+			if bounded_stat.value_changed.is_connected(_set_label_text):
+				bounded_stat.value_changed.disconnect(_set_label_text)
+			if bounded_stat.max_stat.value_changed.is_connected(_set_label_text):
+				bounded_stat.max_stat.value_changed.disconnect(_set_label_text)
 		
 		bounded_stat = p_stat
 		
 		# 2. Setup new stat
 		if bounded_stat:
 			bounded_stat.value_changed.connect(_set_label_text)
+			bounded_stat.max_stat.value_changed.connect(_set_label_text)
 			# Only update if the Label is actually in the tree
 			if is_inside_tree():
 				_set_label_text()
@@ -23,6 +27,6 @@ func _ready() -> void:
 
 func _set_label_text(_p_ignored_value: int = 0) -> void:
 	if bounded_stat:
-		text = str(bounded_stat.max_stat.stat_name, ": ", bounded_stat.max_stat.value, "/", bounded_stat.current_value);
+		text = str(bounded_stat.max_stat.stat_name, ": ", bounded_stat.current_value, "/", bounded_stat.max_stat.value);
 	else:
 		text = "No Stat"
